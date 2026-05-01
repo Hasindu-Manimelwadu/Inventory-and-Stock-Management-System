fd348f64e212050951f793f540242f1666c222e0
Manimelwadu <it25101517@my.sliit.lk>
2026-05-01 11:50:58 +0530

    Add Purchase Order Management module - Hasindu

---
+++ src/main/java/com/example/purchaseordersystem/PurchaseOrderSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.purchaseordersystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PurchaseOrderSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PurchaseOrderSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/purchaseordersystem/PurchaseOrderSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.purchaseordersystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PurchaseOrderSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
