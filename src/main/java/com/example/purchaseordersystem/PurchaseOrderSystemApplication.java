223415c4778b85146c1294dd3d530f154ff21905
Manimelwadu <it25101517@my.sliit.lk>
2026-05-13 12:11:52 +0530

    Fix package scanning for all modules

--- src/main/java/com/example/purchaseordersystem/PurchaseOrderSystemApplication.java
+++ src/main/java/com/example/purchaseordersystem/PurchaseOrderSystemApplication.java
@@ -3,11 +3,12 @@ package com.example.purchaseordersystem;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {
+        "com.example.purchaseordersystem",
+        "com.InventoryManagement"
+})
 public class PurchaseOrderSystemApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(PurchaseOrderSystemApplication.class, args);
     }
-
 }
